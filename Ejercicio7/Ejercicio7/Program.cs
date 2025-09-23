5f5d832fb6187f7db1389adbd6ca1d73288bc175
juanjo210106 <jgamlop2101@g.educaand.es>
2025-09-23 14:10:43 +0200

    Add files via upload

---
+++ Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(":: Introduce el precio del producto...: ");
+            double precio = double.Parse(Console.ReadLine());
+
+            Console.Write(":: Introduce la cantidad total........: ");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            double costeT = precio * cantidad;
+            Console.WriteLine("");
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("TOTAL ES DE: " + costeT);
+        }
+    }
+}
