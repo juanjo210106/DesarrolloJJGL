27bbb9df3086641145ee69d6d436fb801f67b81a
juanjo210106 <jgamlop2101@g.educaand.es>
2025-10-01 14:28:17 +0200

    Add files via upload

---
+++ EJERCICIOS_0110/ejercicio3/ejercicio3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("... Introduce un número para calcular el factorial: ");
+            int num = int.Parse(Console.ReadLine());
+
+            calcularFactorial(num);
+            
+        }
+
+        public static void calcularFactorial(int num)
+        {
+            int resultado = num;
+            Console.Write(num + "! = " + num + " x ");
+            for (int i = num-1; i >= 1; i--)
+            {
+                resultado = resultado * i;
+                if (i!=1)
+                {
+                    Console.Write(i + " x ");
+                } else
+                {
+                    Console.Write(i + " = ");
+                }
+            }
+            Console.Write(resultado);
+        }
+    }
+}
