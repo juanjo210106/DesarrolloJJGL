85876f687d8a4ea0b1ead71afa2ee1818da87789
juanjo210106 <jgamlop2101@g.educaand.es>
2025-11-03 11:04:01 +0100

    Subida Tarea 6 JJGL | 03/11/2025

---
+++ TEMA 3 -WPF/Práctica 6 WPF JJGL/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CutCommandBinding_CanExecute (object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (txtEditor != null) && (txtEditor.SelectionLength > 0);
+        }
+
+        private void CutCommandBinding_Executed (object sender, ExecutedRoutedEventArgs e)
+        {
+            txtEditor.Cut();
+        }
+
+        private void PasteCommandBinding_CanExecute (Object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = Clipboard.ContainsText();
+        }
+
+        private void PasteCommandBinding_Executed (Object sender, ExecutedRoutedEventArgs e)
+        {
+            txtEditor.Paste();
+        }
+    }
+}
---
+++ TEMA 3 -WPF/Práctica 6 WPF JJGL/WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click (object sender, RoutedEventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string email = txtEmail.Text;
+            string mensaje = txtMensaje.Text;
+
+            string textoDelPopup = "El usuario " + nombre + " con email " + email + " envía el mensaje: " + mensaje;
+            string tituloDelPopup = "Formulario Enviado";
+
+            MessageBox.Show(textoDelPopup, tituloDelPopup, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
