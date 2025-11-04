f4780cde03a6dea350a10bf5902581917b4b7562
Juan José Gamero López <jgamlop2101@g.educaand.es>
2025-11-04 13:39:26 +0100

    Subida del ejercicio 4 en la práctica 6

---
+++ TEMA 3 -WPF/Práctica 6 WPF JJGL/WpfApp4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAbrir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OpenCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Archivos de texto (*.txt) | *.txt";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (dialog.ShowDialog() == true)
+            {
+                txtContenido.Text = File.ReadAllText(dialog.FileName);
+            }
+        }
+
+        private void SaveCommand_CanExecute(Object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = txtContenido != null && !string.IsNullOrWhiteSpace(txtContenido.Text);
+        }
+
+
+        private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Archivos de texto (*.txt) | *.txt ";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, txtContenido.Text);
+            }
+        }
+    }
+}
