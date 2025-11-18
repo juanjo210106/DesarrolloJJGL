fb704c83c9bc028d36c43c8cf082a3470144f13a
juanjo210106 <jgamlop2101@g.educaand.es>
2025-11-18 14:35:02 +0100

    Subida ejercicio avanzado WPF

---
+++ TEMA 3 -WPF/Práctica 8 WPF JJGL/NivelAvanzado/NivelAvanzado/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NivelAvanzado
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            Ventana2 ventana = new Ventana2();
+
+            bool? resultado = ventana.ShowDialog();
+
+            if (resultado == true)
+            {
+                string nombre = ventana.NombreRecogido;
+                string edad = ventana.EdadRecogida;
+
+               
+                listBox.Items.Add($"Nombre: {nombre} - Edad: {edad}");
+            }
+        }
+
+        // Botón Salir
+        private void btnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ TEMA 3 -WPF/Práctica 8 WPF JJGL/NivelAvanzado/NivelAvanzado/Ventana2.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NivelAvanzado
+{
+    /// <summary>
+    /// Lógica de interacción para Ventana2.xaml
+    /// </summary>
+    public partial class Ventana2 : Window
+    {
+        public String NombreRecogido;
+        public String EdadRecogida;
+        public Ventana2()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Aceptar(object sender, RoutedEventArgs e)
+        {
+
+            NombreRecogido = txtNombre.Text;
+            EdadRecogida = txtEdad.Text;
+
+
+            this.DialogResult = true;
+            this.Close();
+
+        }
+
+        
+        private void btn_Cancelar(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
