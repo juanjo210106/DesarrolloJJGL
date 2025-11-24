b844a154b6118ba64c76f6ae85c8dda145ed5d5f
juanjo210106 <jgamlop2101@g.educaand.es>
2025-11-24 11:00:23 +0100

    Subida de la práctica 10 WPF

---
+++ TEMA 3 -WPF/Práctica 10 WPF JJGL/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtDni_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            String[] letras = {"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"};
+            String dni = txtDni.Text;
+
+            if (dni.Length == 8)
+            {
+                int resto = int.Parse(dni) % 23;
+                txtSalida.Text = "La letra del DNI es la " + letras[resto];
+            } else
+            {
+                txtSalida.Text = "Introduce un DNI para comprobar la letra...";
+            }
+        }
+    }
+}
---
+++ TEMA 3 -WPF/Práctica 10 WPF JJGL/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int[,] matriz;
+        int n; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInicializarMatriz(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(dimension.Text, out n))
+            {
+                matriz = new int[n, n];
+                Random rnd = new Random();
+
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                        matriz[i, j] = rnd.Next(0, 100);
+
+                txtSalida.Text = "Matriz generada. Pulsa Imprimir.";
+            }
+        }
+
+
+        String s = "";
+        private void btnImprimirMatriz(object sender, RoutedEventArgs e)
+        {
+            if (matriz == null) return;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    s += matriz[i, j] + "\t";
+                s += "\n";
+            }
+            txtSalida.Text = s;
+        }
+
+        private void btnImprimirTraspuesta(object sender, RoutedEventArgs e)
+        {
+            if (matriz == null) return;
+            string s = "MATRIZ TRASPUESTA:\n";
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    s += matriz[j, i] + "\t";
+                s += "\n";
+            }
+            txtSalida.Text = s;
+        }
+    }
+}
