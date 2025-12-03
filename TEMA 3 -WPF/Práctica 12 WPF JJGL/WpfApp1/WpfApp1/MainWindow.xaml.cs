690d8ff5d3f53fb7de4ac0a48575861754658db3
juanjo210106 <jgamlop2101@g.educaand.es>
2025-12-03 14:41:39 +0100

    Subida ejercicio 1 practica 12

--- TEMA 3 -WPF/Práctica 12 WPF JJGL/WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ TEMA 3 -WPF/Práctica 12 WPF JJGL/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Configuration;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,33 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TabItem_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            string num1 = txtValor1.Text;
+            string num2 = txtValor2.Text;
+
+            if (num1 != "" || num2 != "")
+            {
+                double n1 = double.Parse(num1);
+                double n2 = double.Parse(num2);
+                if (n1 < 0 || n2 < 0)
+                {
+                    MessageBox.Show("Se ha producido un error... Los campos debén estar rellenos por un número 0 o más grande de 0", "Error");
+                }
+                else
+                {
+                    double result = n1 * n2;
+                    txtResultado.Text = result.ToString();
+                }
+
+
+            }
+            else
+            {
+                MessageBox.Show("Los valores no pueden estar vacíos... vuelve a introducirlos para intentarlo de nuevo", "Advertencia");
+            }
         }
     }
 }
