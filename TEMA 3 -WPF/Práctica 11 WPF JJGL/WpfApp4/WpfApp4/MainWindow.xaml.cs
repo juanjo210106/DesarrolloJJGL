5ddac2401523158d055d9ce10b7c5357351c9511
juanjo210106 <jgamlop2101@g.educaand.es>
2025-12-03 13:26:57 +0100

    Subida de Ejercicios - Práctica 11
    Subida del ejercicio 3 y 4 de la práctica 11 de WPF

---
+++ TEMA 3 -WPF/Práctica 11 WPF JJGL/WpfApp4/WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Anterior_Click(object sender, RoutedEventArgs e)
+        {
+            if (Tabs.SelectedIndex > 0)
+                Tabs.SelectedIndex--;
+        }
+
+        private void Siguiente_Click(object sender, RoutedEventArgs e)
+        {
+            if (Tabs.SelectedIndex < Tabs.Items.Count - 1)
+                Tabs.SelectedIndex++;
+        }
+
+        private void Seleccionado_Click(object sender, RoutedEventArgs e)
+        {
+            var tab = (Tabs.SelectedItem as System.Windows.Controls.TabItem);
+            string nombre = tab.Header.ToString();
+
+            MessageBox.Show($"Has seleccionado la opción de {nombre}");
+        }
+    }
+}
