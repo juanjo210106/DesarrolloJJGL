8cd9096ca77246115f2202ef9e79eef88891f790
juanjo210106 <jgamlop2101@g.educaand.es>
2025-12-10 14:34:49 +0100

    Subida del ejercicio de simulación para el examen

---
+++ TEMA 3 -WPF/SimulacroExamen/WpfApp1/WpfApp1/Ventana2.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para Ventana2.xaml
+    /// </summary>
+    public partial class Ventana2 : Window
+    {
+        public Ventana2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtNombre.Text != "" && txtDni.Text != "" && txtEdad.Text != "")
+            {
+                MessageBox.Show("Has añadido correctamente el usuario...", "Información");
+
+
+                this.Close();
+
+            }
+        }
+    }
+}
---
+++ TEMA 3 -WPF/SimulacroExamen/WpfApp1/WpfApp1/Ventana3.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para Ventana3.xaml
+    /// </summary>
+    public partial class Ventana3 : Window
+    {
+        public Ventana3()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_baloncesto(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionado baloncesto");
+            this.Close();
+        }
+
+        private void btn_Futbol(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Has seleccionado Futbol");
+            this.Close();
+        }
+    }
+}
