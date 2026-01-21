93e827c033734de2871ac435aa57f69cd6c9066f
juanjo210106 <jgamlop2101@g.educaand.es>
2026-01-21 13:47:57 +0100

    Subida de la primera app

---
+++ TEMA 7 - NETMAUI/MiPrimeraApp/MiPrimeraApp/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MiPrimeraApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+Creación del tema  7}
---
+++ TEMA 7 - NETMAUI/MiPrimeraApp/MiPrimeraApp/NewPage1.xaml.cs
@@ -0,0 +1,11 @@
+namespace MiPrimeraApp;
+
+public partial class NewPage1 : ContentPage
+{
+	public NewPage1()
+	{
+		InitializeComponent();
+
+        
+	}
+}
