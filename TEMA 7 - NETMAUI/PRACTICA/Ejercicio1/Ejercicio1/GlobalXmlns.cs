cd7db365bdbc90d57bedc82d1cda1ea073305e92
juanjo210106 <jgamlop2101@g.educaand.es>
2026-02-02 11:12:11 +0100

    Subida Ejercicio 1

---
+++ TEMA 7 - NETMAUI/PRACTICA/Ejercicio1/Ejercicio1/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Ejercicio1")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Ejercicio1.Pages")]
