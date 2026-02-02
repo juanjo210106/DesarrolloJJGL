93b2739e2985ab614ac96a13233e24eac3ef1a46
juanjo210106 <jgamlop2101@g.educaand.es>
2026-02-02 10:43:43 +0100

    Subida de ejercicio 1

---
+++ TEMA 7 - NETMAUI/Ejercicio1/Ejercicio1/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace Ejercicio1
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnLoginClicked(object sender, EventArgs e)
+        {
+            
+            await Navigation.PushAsync(new PerfilUsuario());
+        }
+    }
+}
---
+++ TEMA 7 - NETMAUI/Ejercicio1/Ejercicio1/PerfilUsuario.xaml.cs
@@ -0,0 +1,9 @@
+namespace Ejercicio1;
+
+public partial class PerfilUsuario : ContentPage
+{
+	public PerfilUsuario()
+	{
+		InitializeComponent();
+	}
+}
